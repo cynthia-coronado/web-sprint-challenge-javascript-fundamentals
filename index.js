93643642ee9b9a30728447a3485c9b50315c3e55
Cynthia Coronado <cynthiacoronado@Cynthias-Air.hsd1.ca.comcast.net>
2021-02-20 14:44:26 -0800

    Topic three step three done

--- index.js
+++ index.js
@@ -163,7 +163,9 @@ CuboidMaker.prototype.volume = function () {
   Create another method called surfaceArea using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
   Formula for cuboid surface area of a cube: 
   2 * (length * width + length * height + width * height)  */
-
+CuboidMaker.prototype.surfaceArea = function () {
+  return 2 * (this.length * this.width * this.length * this.height * this.width * this.height);
+}
 
 /* 🐴🐴🐴 Step 4: Create a new object that uses CuboidMaker 🐴🐴🐴
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
